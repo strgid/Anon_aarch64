15818d486ad2fb4f9f223ea1ce85832c63b6c029
MarkCup <1586827710@qq.com>
2025-02-03 20:42:55 +0800

    切换技能逻辑

--- Assets/Scripts/States/AnonState.cs
+++ Assets/Scripts/States/AnonState.cs
@@ -6,11 +6,15 @@ public class AnonState : State
 {
     int counter;
 
+    int totalHit = 0;
+
+    public State[] States;
+
     public override void Init(GameManager gm)
     {
         base.Init(gm);
         counter = 0;
-        life=4;
+        life = 4;
     }
 
     public override void Generate()
@@ -33,5 +37,26 @@ public class AnonState : State
                 success = h.GenerateAnon(moles[4], 0, this);
             }
         }
+    }
+
+    public int HitOverThanSwitch = 8;
+
+    public override State GetNextState()
+    {
+        if (totalHit >= HitOverThanSwitch)
+        {
+            totalHit = 0;
+            return States[Random.Range(0, States.Length)];
+        }
+        else
+        {
+            return this;
+        }
+    }
+
+    public override void Hit(int id)
+    {
+        base.Hit(id);
+        totalHit++;
     }
 }
